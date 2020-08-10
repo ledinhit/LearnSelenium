76a791517b27e1d3584a26b5e66e916069e60094
LeDinh <LeDinh@D1111.MASTER.local>
2020-08-10 11:31:09 +0700

    add first commit

---
+++ src/DemoSelenum/Test_01_Open_Browser.java
@@ -0,0 +1,40 @@
+package DemoSelenum;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test_01_Open_Browser {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "F:\\DemoSelenium\\Browser\\chromedriver.exe");
+		// System.setProperty("webdriver.gecko.driver","F:\\DemoSelenium\\Browser\\geckodriver.exe");
+		// driver = new FirefoxDriver();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://app.omisell.com/authentication/login");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		//// String loginPageUrl = driver.getCurrentUrl();
+		// System.out.println(loginPageUrl);
+		// Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
